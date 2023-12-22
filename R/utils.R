102bab27269a137b5eff00d8e7ac94095f0aadd4
yun <yunyunpp96@outlook.com>
2023-12-23 00:15:07 +0800

    export format_num

--- R/utils.R
+++ R/utils.R
@@ -13,7 +13,12 @@ NULL
 #'
 #' @param x An atomic vector
 #' @return A character vector containing the formatted x
+#' @export
 format_num <- function(x) {
-  assert_(x, is.numeric, "numeric")
-  data.table::fifelse(x < 0.01, sprintf("%1.2e", x), sprintf("%.3f", x))
+    assert_(x, is.numeric, "numeric")
+    data.table::fifelse(
+        x < 0.01,
+        sprintf("%.2e", x),
+        sprintf("%.3f", x)
+    )
 }
